46f92a889abc7c386d29eff2a59360518a758160
Andrew Thomas <andrewt@gmail.com>
2026-08-13 16:06:17 -0400

    clean up heading

--- app/page.tsx
+++ app/page.tsx
@@ -392,9 +392,8 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center gap-10 py-32 px-16 bg-white dark:bg-black">
         <h1 className="text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-          Juno Home Loans Demo
+          Fido Home Loans Demo
         </h1>
-        <h3>This is a demo. Not a real site</h3>
 
         {step === "interest" && (
           <QuestionCard
